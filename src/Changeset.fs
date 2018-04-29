9ee8a04e292e375859b906678acc1f64455ae8c1
Michel Perez <michel.ingesoft@gmail.com>
2018-04-29 16:15:42 -0500

    wip

--- src/Changeset.fs
+++ src/Changeset.fs
@@ -1,7 +1,15 @@
 namespace Femto
 
 module Changeset =
-    type ValidateFunc<'T> = 'T -> bool
+
+    type Validation<'T> = {
+        func: ValidateFunc<'T>
+        field: FieldName
+        errMsg: ErrMessage
+    }
+    and FieldName = FieldName of string
+    and ErrMessage = ErrMessage of string
+    and ValidateFunc<'T> = 'T -> bool
 
     type Changeset<'T> = {
         data: 'T
@@ -9,14 +17,13 @@ module Changeset =
         parameters: Map<string, obj>
         errors: Map<string, string>
         valid: Option<bool>
-        validations: List<ValidateFunc<'T>>
+        validations: List<Validation<'T>>
     }
 
     let cast (parameters: Map<string, obj>) (attributes: List<string>) (model: 'T) =
-        let filter k _ =
-            List.contains k attributes
-        let valuesFiltered =
-            Map.filter filter parameters
+        let filter k _ = List.contains k attributes
+        let valuesFiltered = Map.filter filter parameters
+
         {
             data = TypeUtils.updateModel valuesFiltered model
             changes = valuesFiltered
@@ -26,12 +33,26 @@ module Changeset =
             validations = []
         }
 
+    let addValidation func fieldName errMsg changeset =
+        let validation = {
+            func = func
+            field = FieldName fieldName
+            errMsg = ErrMessage errMsg
+        }
+
+        { changeset with validations = validation :: changeset.validations }
+
     let validate changeset =
-        let valid =
+        let foldFunc
+            (valid, errors)
+            { func = func; field = FieldName fieldName; errMsg = ErrMessage errMsg } =
+            if func changeset.data then
+                (false, Map.add fieldName errMsg errors)
+            else
+                (valid, errors)
+
+        let (valid, errors) =
             changeset.validations
-            |> List.fold
-                (fun valid validation ->
-                    if valid then validation changeset.data
-                    else valid)
-                false
-        { changeset with valid = Some valid }
+            |> List.fold foldFunc (true, Map.empty)
+
+        { changeset with valid = Some valid; errors = errors }
