74ff55c5e6ca33014b6e637bb4ab16721af3c697
Michel Perez <michel.ingesoft@gmail.com>
2018-04-28 23:23:45 -0500

    wip

--- src/Changeset.fs
+++ src/Changeset.fs
@@ -1,30 +1,37 @@
 namespace Femto
-open System
-open System.ComponentModel
 
 module Changeset =
+    type ValidateFunc<'T> = 'T -> bool
+
     type Changeset<'T> = {
         data: 'T
-        changes: Map<string, string>
+        changes: Map<string, obj>
         parameters: Map<string, obj>
         errors: Map<string, string>
-        valid: bool
+        valid: Option<bool>
         validations: List<ValidateFunc<'T>>
     }
-    and ValidateFunc<'T> =
-            Changeset<'T> -> Changeset<'T>
 
     let cast (parameters: Map<string, obj>) (attributes: List<string>) (model: 'T) =
         let filter k _ =
             List.contains k attributes
         let valuesFiltered =
             Map.filter filter parameters
-
         {
             data = TypeUtils.updateModel valuesFiltered model
-            changes = Map.empty
+            changes = valuesFiltered
             parameters = parameters
             errors = Map.empty
-            valid = false
+            valid = None
             validations = []
         }
+
+    let validate changeset =
+        let valid =
+            changeset.validations
+            |> List.fold
+                (fun valid validation ->
+                    if valid then validation changeset.data
+                    else valid)
+                false
+        { changeset with valid = Some valid }
