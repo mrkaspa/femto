a7ecbe1ee3849870e4efa8723095e6bd18936047
Michel Perez <michel.ingesoft@gmail.com>
2018-05-04 17:43:03 -0500

    model example in test

--- test/ChangesetTests.fs
+++ test/ChangesetTests.fs
@@ -1,37 +1,43 @@
 module ChangesetTests
 
 open Expecto
-open Femto
 
-type User = {
-    name: string
-    age: int
-}
+module UserSchema =
+    open Femto
+
+    type User = {
+        name: string
+        age: int
+    }
+
+    let changeset model parameters  =
+        model
+        |> Changeset.cast parameters ["name"]
+        |> Changeset.addValidation
+            (fun u -> u.age < 20)
+            "age"
+            "should be greater than 20"
+        |> Changeset.addValidation
+            (fun u -> u.name <> "")
+            "name"
+            "should not be empty"
+        |> Changeset.validate
+
+open UserSchema
 
 let tests =
     testList "Changeset" [
         testCase "Updates the data on cast" <| fun _ ->
-            let user = { name = ""; age = 20 }
+            let user = { name = "demo"; age = 21 }
             let parameters = Map.ofList ["name", "michel" :> obj]
-            let ch = Changeset.cast parameters ["name"] user
+            let ch = UserSchema.changeset user parameters
             Expect.isTrue (ch.data.name = "michel") "changes name"
-            Expect.isTrue (user.name = "") "does not change name"
+            Expect.isTrue (user.name = "demo") "does not change name"
 
         testCase "Validates a changeset" <| fun _ ->
             let user = { name = ""; age = 18 }
             let parameters = Map.ofList ["name", "michel" :> obj]
-            let ch =
-                user
-                |> Changeset.cast parameters ["name"]
-                |> Changeset.addValidation
-                    (fun u -> u.age < 20)
-                    "age"
-                    "should be greater than 20"
-                |> Changeset.addValidation
-                    (fun u -> u.name <> "")
-                    "name"
-                    "should not be empty"
-                |> Changeset.validate
+            let ch = UserSchema.changeset user parameters
             Expect.isFalse ch.valid.Value "invalid changeset"
             Expect.isNonEmpty ch.errors "must have errors"
             Expect.isTrue (Map.count ch.errors = 2) "has 2 errors"
